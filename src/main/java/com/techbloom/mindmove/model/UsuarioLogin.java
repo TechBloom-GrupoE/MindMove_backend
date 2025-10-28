e970fadc027b69060fce2d7812fdc2fda0fae750
Bianca Maria <biancams_@outlook.com>
2025-10-28 14:31:01 -0300

    :sparkles: Criação do Recurso Usuario

---
+++ src/main/java/com/techbloom/mindmove/model/UsuarioLogin.java
@@ -0,0 +1,74 @@
+package com.techbloom.mindmove.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class UsuarioLogin {
+
+	private Long id;
+	private String nome;
+	private String usuario;
+	private LocalDate data_nasc;
+	private BigDecimal altura;
+	private BigDecimal peso;
+	private String senha;
+	private String foto;
+	private String token;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public LocalDate getData_nasc() {
+		return data_nasc;
+	}
+	public void setData_nasc(LocalDate data_nasc) {
+		this.data_nasc = data_nasc;
+	}
+	public BigDecimal getAltura() {
+		return altura;
+	}
+	public void setAltura(BigDecimal altura) {
+		this.altura = altura;
+	}
+	public BigDecimal getPeso() {
+		return peso;
+	}
+	public void setPeso(BigDecimal peso) {
+		this.peso = peso;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+	
+}
