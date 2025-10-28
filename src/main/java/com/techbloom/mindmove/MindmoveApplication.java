04409c0cac291f35c074bd522332f734b3e585d1
Bianca Maria <biancams_@outlook.com>
2025-10-28 10:55:36 -0300

    :tada: Commit Inicial Projeto MindMove

---
+++ src/main/java/com/techbloom/mindmove/MindmoveApplication.java
@@ -0,0 +1,13 @@
+package com.techbloom.mindmove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindmoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindmoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techbloom/mindmove/MindmoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbloom.mindmove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindmoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
